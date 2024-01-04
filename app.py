8c860f6803f3a61bf4dad1e5633f30bb799b2324
shreyasvidhate <87441989+shreyasvidhate@users.noreply.github.com>
2024-01-05 01:29:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 @app.get("/")
 def index():
     return {"name":"Shrey"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
